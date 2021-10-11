ca6195c5da2d60b02ad75859f95c5bd5c87630b0
mayank <mayank.agarwal0903@gmail.com>
2021-10-11 06:00:11 +0530

    Created the Game class with the UI

---
+++ game.rb
@@ -0,0 +1,38 @@
+require_relative "board.rb"
+class Game
+    def initialize(size, bombs)
+        @board= Board.new(size,bombs)
+    end
+
+    def game_over?
+        until @board.win? || @board.lose?
+            puts @board.render
+
+            action, pos= get_move
+            perform_action(action,pos)
+        end
+
+        if @board.win?
+            puts "YOU WON!!!!"
+        elsif @board.lose?
+            puts @board.reveal
+        end
+    end
+
+    def get_move
+        puts "Please enter the action you want to perform and position where you intend to perform it."
+        inp= gets.chomp.split(",")
+        [inp[0],[inp[1].to_i, inp[2].to_i]]
+    end
+
+    def perform_action(action, pos)
+        tile= @board[pos]
+        
+        case action
+        when 'f'
+            tile.toggle_flag
+        when 'e'
+            tile.explore
+        end
+    end
+end
